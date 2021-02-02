5cd6c6e4b50bc6afad213d4779dccd60e7914bcb
HuangZhiMing <huangzhimingme@gmail.com>
2021-02-02 21:03:01 +0800

    多线程:Volatile停止线程的BUG

--- multithreading/src/main/java/pers/zhiming/javalab/multithreading/stopthread/volatiledemo/WrongWayVolatileCannotStop.java
+++ multithreading/src/main/java/pers/zhiming/javalab/multithreading/stopthread/volatiledemo/WrongWayVolatileCannotStop.java
@@ -1,6 +1,7 @@
 package pers.zhiming.javalab.multithreading.stopthread.volatiledemo;
 
-import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * 演示用volatile的局限
@@ -8,18 +9,64 @@ import java.util.concurrent.BlockingDeque;
  */
 public class WrongWayVolatileCannotStop {
 
+    public static void main(String[] args) throws InterruptedException {
+        ArrayBlockingQueue storage = new ArrayBlockingQueue(10);
+        Producer producer = new Producer(storage);
+        Thread producerThread = new Thread(producer);
+        producerThread.start();
+        Thread.sleep(1000);
+        Consumer consumer = new Consumer(storage);
+        while (consumer.needMoreNums()) {
+            System.out.println(consumer.storage.take() + "被消费了");
+            Thread.sleep(100);
+        }
+        System.out.println("消费者不需要更多数据了。");
+        producer.canceled = true;
+    }
 }
 
 class Producer implements Runnable {
 
-    BlockingDeque storage;
+    public volatile boolean canceled = false;
 
-    public Producer(BlockingDeque storage) {
+    BlockingQueue storage;
+
+    public Producer(BlockingQueue storage) {
         this.storage = storage;
     }
 
     @Override
     public void run() {
+        int num = 0;
+        try {
+            while (num <= 100000 && !canceled) {
+                if (num % 100 == 0) {
+                    storage.put(num);
+                    System.out.println(num + "是100的倍数,被放到仓库中。");
+                }
+                num++;
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("生产者结束运行");
+        }
+    }
+}
 
+class Consumer {
+
+    BlockingQueue storage;
+
+    public Consumer(BlockingQueue storage) {
+        this.storage = storage;
+    }
+
+    public boolean needMoreNums() {
+        if (Math.random() > 0.95) {
+            return false;
+        }
+        return true;
     }
 }
---
+++ multithreading/src/main/java/pers/zhiming/javalab/multithreading/stopthread/volatiledemo/WrongWayVolatileFixed.java
@@ -0,0 +1,70 @@
+package pers.zhiming.javalab.multithreading.stopthread.volatiledemo;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class WrongWayVolatileFixed {
+
+    public static void main(String[] args) throws InterruptedException {
+        WrongWayVolatileFixed body = new WrongWayVolatileFixed();
+        ArrayBlockingQueue storage = new ArrayBlockingQueue(10);
+        Producer producer = body.new Producer(storage);
+        Thread producerThread = new Thread(producer);
+        producerThread.start();
+        Thread.sleep(1000);
+        Consumer consumer = body.new Consumer(storage);
+        while (consumer.needMoreNums()) {
+            System.out.println(consumer.storage.take() + "被消费了");
+            Thread.sleep(100);
+        }
+        System.out.println("消费者不需要更多数据了。");
+        producerThread.interrupt();
+    }
+
+    class Producer implements Runnable {
+
+        public volatile boolean canceled = false;
+
+        BlockingQueue storage;
+
+        public Producer(BlockingQueue storage) {
+            this.storage = storage;
+        }
+
+        @Override
+        public void run() {
+            int num = 0;
+            try {
+                while (num <= 100000 && !Thread.currentThread().isInterrupted()) {
+                    if (num % 100 == 0) {
+                        storage.put(num);
+                        System.out.println(num + "是100的倍数,被放到仓库中。");
+                    }
+                    num++;
+                    Thread.sleep(1);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("生产者结束运行");
+            }
+        }
+    }
+
+    class Consumer {
+
+        BlockingQueue storage;
+
+        public Consumer(BlockingQueue storage) {
+            this.storage = storage;
+        }
+
+        public boolean needMoreNums() {
+            if (Math.random() > 0.95) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+}
